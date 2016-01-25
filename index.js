cf996948510e85fb95994d7f338960109d8e6683
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-25 18:39:56 +0200

    fix

--- index.js
+++ index.js
@@ -1,91 +1,19 @@
-var _ = require('lodash');
-var SpotifyWebApi = require('spotify-web-api-node');
+var _ = require('lodash'),
+    util = require('./util.js'),
+    SpotifyWebApi = require('spotify-web-api-node');
 
-var globalPickResult = {
-    'body.artists': {
-        key: 'body',
-        fields: {
-            name: 'Name',
-            popularity: 'Popularity',
-            external_urls: 'URL',
-            followers: 'Followers',
-            uri: 'URI'
-        }
-    }
-};
+var pickInputs = {
+        'id': { key: 'id', validate: { req: true } }
+    },
+    pickOutputs = {
+        'name': { key: 'body.artists', fields: ['name'] },
+        'popularity': { key: 'body.artists', fields: ['popularity'] },
+        'external_urls': { key: 'body.artists', fields: ['external_urls.spotify'] },
+        'followers': { key: 'body.artists', fields: ['followers'] },
+        'uri': { key: 'body.artists', fields: ['uri'] }
+    };
 
 module.exports = {
-    /**
-     * Return pick result.
-     *
-     * @param output
-     * @param pickResult
-     * @returns {*}
-     */
-    pickResult: function (output, pickResult) {
-        var result = {};
-
-        _.map(_.keys(pickResult), function (resultVal) {
-
-            if (_.has(output, resultVal)) {
-
-                if (_.isObject(pickResult[resultVal])) {
-                    if (_.isArray(_.get(output, resultVal))) {
-
-                        if (!_.isArray(result[pickResult[resultVal].key])) {
-                            result[pickResult[resultVal].key] = [];
-                        }
-
-                        _.map(_.get(output, resultVal), function (inOutArrayValue) {
-
-                            result[pickResult[resultVal].key].push(this.pickResult(inOutArrayValue, pickResult[resultVal].fields));
-                        }, this);
-                    } else if (_.isObject(_.get(output, resultVal))){
-
-                        result[pickResult[resultVal].key] = this.pickResult(_.get(output, resultVal), pickResult[resultVal].fields);
-                    }
-                } else {
-                    _.set(result, pickResult[resultVal], _.get(output, resultVal));
-                }
-            }
-        }, this);
-
-        return result;
-    },
-
-    /**
-     * Set acess token.
-     *
-     * @param dexter
-     * @param spotifyApi
-     */
-    authParams: function (dexter, spotifyApi) {
-
-        if (dexter.environment('spotify_access_token')) {
-
-            spotifyApi.setAccessToken(dexter.environment('spotify_access_token'));
-        }
-    },
-
-    /**
-     * Set failure response.
-     *
-     * @param err
-     * @param dexter
-     */
-    failureProcess: function (err, dexter) {
-
-        var result = _.isArray(err)? err : [err];
-
-        if (!dexter.environment('spotify_access_token')) {
-            var envError = 'This module need optional environment variable [spotify_access_token];';
-
-            result.unshift(envError);
-        }
-
-        return result;
-    },
-
     /**
      * The main entry point for the Dexter module.
      *
@@ -93,18 +21,21 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var spotifyApi = new SpotifyWebApi(),
+            token = dexter.provider('spotify').credentials('access_token'),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        var spotifyApi = new SpotifyWebApi();
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-        this.authParams(dexter, spotifyApi);
-
-        spotifyApi.getArtistRelatedArtists(step.input('id').first())
+        spotifyApi.setAccessToken(token);
+        spotifyApi.getArtistRelatedArtists(inputs.id)
             .then(function(data) {
 
-                this.complete(this.pickResult(data, globalPickResult));
+                this.complete(util.pickOutputs(data, pickOutputs));
             }.bind(this), function(err) {
-
-                this.fail(this.failureProcess(err, dexter));
+                this.fail(errc);
             }.bind(this));
     }
 };
